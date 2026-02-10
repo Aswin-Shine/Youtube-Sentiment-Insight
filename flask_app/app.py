ad3ce51eee2b293c24b32bbbd62c915a600d892d
Aswin Shine <ashwinsh.91@gmail.com>
2026-02-11 03:21:07 +0530

    Added Full Continous Deployment

--- flask_app/app.py
+++ flask_app/app.py
@@ -50,39 +50,39 @@ def preprocess_comment(comment):
 
 
 
-# Load the model and vectorizer from the model registry and local storage
-# def load_model_and_vectorizer(model_name, model_version, vectorizer_path):
-#     # Set MLflow tracking URI to your server
-#     mlflow.set_tracking_uri("http://ec2-54-167-108-249.compute-1.amazonaws.com:5000/")  # Replace with your MLflow tracking URI
-#     client = MlflowClient()
-#     model_uri = f"models:/{model_name}/{model_version}"
-#     model = mlflow.pyfunc.load_model(model_uri)
-#     with open(vectorizer_path, 'rb') as file:
-#         vectorizer = pickle.load(file)
+Load the model and vectorizer from the model registry and local storage
+def load_model_and_vectorizer(model_name, model_version, vectorizer_path):
+         # Set MLflow tracking URI to your server
+     mlflow.set_tracking_uri("http://16.170.245.75:5000/")  # Replace with your MLflow tracking URI
+     client = MlflowClient()
+     model_uri = f"models:/{model_name}/{model_version}"
+     model = mlflow.pyfunc.load_model(model_uri)
+     with open(vectorizer_path, 'rb') as file:
+         vectorizer = pickle.load(file)
    
-#     return model, vectorizer
+     return model, vectorizer
 
 
 
-def load_model(model_path, vectorizer_path):
-    """Load the trained model."""
-    try:
-        with open(model_path, 'rb') as file:
-            model = pickle.load(file)
+#def load_model(model_path, vectorizer_path):
+    #"""Load the trained model."""
+    #try:
+        #with open(model_path, 'rb') as file:
+            #model = pickle.load(file)
         
-        with open(vectorizer_path, 'rb') as file:
-            vectorizer = pickle.load(file)
+        #with open(vectorizer_path, 'rb') as file:
+            #vectorizer = pickle.load(file)
       
-        return model, vectorizer
-    except Exception as e:
-        raise
+        #return model, vectorizer
+    #except Exception as e:
+       # raise
 
 
 # Initialize the model and vectorizer
-model, vectorizer = load_model("./lgbm_model.pkl", "./tfidf_vectorizer.pkl")  
+#model, vectorizer = load_model("./lgbm_model.pkl", "./tfidf_vectorizer.pkl")  
 
 # Initialize the model and vectorizer
-# model, vectorizer = load_model_and_vectorizer("my_model", "1", "./tfidf_vectorizer.pkl")  # Update paths and versions as needed
+ model, vectorizer = load_model_and_vectorizer("yt_chrome_plugin_model", "2", "./tfidf_vectorizer.pkl")  # Update paths and versions as needed
 
 @app.route('/')
 def home():
